31f032657ddaa3b7e4001c482914fdb9588ff568
Tri Wanda Septian <9025301+twseptian@users.noreply.github.com>
2025-10-22 16:22:50 +0700

    update post
    update post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761124195',resources: [
+    cacheName: 'chirpy-1761124915',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
