31521a580e0db2fe9941c8a495708253651d92c8
Tri Wanda Septian <9025301+twseptian@users.noreply.github.com>
2025-10-22 16:04:14 +0700

    add a new post
    add a new post mcp jadx-gui

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761058854',resources: [
+    cacheName: 'chirpy-1761123788',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
