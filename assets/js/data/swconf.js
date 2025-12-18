4063e0cbddf9ef85a9c53e42460918cba5c44810
Tri Wanda Septian <9025301+twseptian@users.noreply.github.com>
2025-12-18 08:48:21 +0700

    update categories
    some of misconfiguration

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1765875747',resources: [
+    cacheName: 'chirpy-1766022394',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
